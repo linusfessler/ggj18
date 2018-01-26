e5926956e2801929878b0fa1f1486ea98edded90
Linus <linusfessler@gmail.com>
2018-01-27 00:33:04 +0100

    Movement

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+	const float gravity = 0.01f;
+
+	void Start() {
+		
+	}
+
+	void Update() {
+		
+	}
+
+	void OnTriggerStay(Collider collider) {
+		Movement movement = collider.GetComponent<Movement>();
+		if (movement) {
+			movement.AddVelocity(gravity * (transform.position - collider.transform.position));
+		}
+	}
+}
