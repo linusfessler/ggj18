867eabba4cf763a6b8307d1787e4d22eb766454c
Moritzjenny <moritzjenny@hotmail.com>
2018-01-27 23:44:03 +0100

    Startscreen

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	[SerializeField] float angularVelocity = 1f;
+
+	void Start () {
+		GetComponent<Rigidbody> ().angularVelocity = angularVelocity * Vector3.up;
+	}
+}
