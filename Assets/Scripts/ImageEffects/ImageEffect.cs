e71e1b49e1f968a1e63625484f29035ff0f05e5a
Jan Carlo Schneider <junk.u.spam@gmail.com>
2018-01-27 02:16:28 +0100

    vrlauf textur und image efect imrpved

--- Assets/Scripts/ImageEffects/ImageEffect.cs
+++ Assets/Scripts/ImageEffects/ImageEffect.cs
@@ -3,19 +3,37 @@
 [ExecuteInEditMode]
 public class ImageEffect : MonoBehaviour {
 
-    public Material EffectMaterial;
+    [Range(0, 1)]
+    public float intensity = 0;
+    private float lastFrameIntensity;
+    public Material ColorShift;
     public Material BlockDistort;
 
 
     private RenderTexture lastFrame;
-    private Texture tempTex;
+    private RenderTexture tempTex;
 
     private void OnRenderImage(RenderTexture src, RenderTexture dst)
     {
-        Graphics.Blit(src, dst, BlockDistort);
+        tempTex = new RenderTexture(512, 512, 16, RenderTextureFormat.ARGB32);
+        tempTex.Create();
+        Graphics.Blit(src, tempTex, ColorShift);
+        Graphics.Blit(tempTex, dst, BlockDistort);
     }
 
-    private void DataMosh() {
+    private void Start()
+    {
+        lastFrameIntensity = intensity;
+    }
+
+    private void Update()
+    {
+        if (Mathf.Abs(intensity - lastFrameIntensity) > 0.01) {
+            UpdateIntensity();
+        }
+    }
+
+    private void UpdateIntensity() {
 
     }
 }
